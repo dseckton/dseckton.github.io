c7b3b69e7eb6698f3c5ff98f93b4e6024261f067
dseckton <eck15001@byui.edu>
2019-10-24 10:32:27 -0600

    touch stuff

--- ta_cit260/sandbox/touchtest.js
+++ ta_cit260/sandbox/touchtest.js
@@ -1,5 +1,16 @@
 const touchMe = document.getElementById("touchMe");
 
 touchMe.addEventListener('touchstart', function() {
+    touchMe.classList.remove('untouch', 'touching');
     touchMe.classList.add('touched');
+})
+
+touchMe.addEventListener('touchmove', function() {
+    touchMe.classList.remove('touched', 'untouch');
+    touchMe.classList.add('touching');
+})
+
+touchMe.addEventListener('touchend', function {
+    touchMe.classList.remove('touching', 'touched');
+    touchMe.classList.add('untouch');
 })
